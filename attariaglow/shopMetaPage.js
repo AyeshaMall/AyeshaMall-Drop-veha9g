e4a9ea934f856228e41ba00ed6059b23fffe3baa
AyeshaMall <buykiamarketplace@gmail.com>
2026-07-12 18:10:37 +0500

    Upload: attariaglow/shopMetaPage.js

---
+++ attariaglow/shopMetaPage.js
@@ -0,0 +1,31 @@
+// shopMeta.js - AyeshaMall custom data manager
+export const shopMeta = {
+  "website": {
+    "name": "Attaria Glow Shop ",
+    "tagline": "Premium Beauty & Skincare Essentials 💖",
+    "description": "Welcome to Attaria Glow Shop! We provide premium quality skincare, cosmetics, and beauty essentials designed to give your skin a natural, healthy glow. From highly effective syrups like Slim Pro to viral beauty products, we ensure 100% authenticity, safe packaging, and fast delivery across Pakistan. Partner with us for the best wholesale rates and top-notch customer satisfaction!",
+    "logo": "image/logo.png"
+  },
+  "about": {
+    "title": "About Attaria Glow Shop ",
+    "description": "Welcome to Attaria Glow Shop! We sell high-quality skincare and beauty products in Pakistan. We have 100% original products like our popular Slim Pro Syrup and cosmetics to give your skin a natural glow. We provide fast shipping, safe packing, and the best wholesale prices for resellers. Shop with confidence!"
+  },
+  "contact": {
+    "title": "📞 Contact Us",
+    "email": "nimraanwar0404@gmail.com",
+    "phone": "03258145213",
+    "address": "Lahore, panjab ,Pakistan ",
+    "social": {
+      "facebook": "https://www.facebook.com/BeautyHubAttaria?mibextid=ZbWKwL",
+      "instagram": "https://instagram.com/8thwonder",
+      "whatsapp": "https://wa.me/923258145213"
+    }
+  },
+  "privacyPolicy": {
+    "title": "🔒 Privacy Policy",
+    "content": "We respect your privacy. Your data is safe with us. We collect only necessary info to process orders. No third-party sharing. Secure encrypted transactions."
+  }
+};
+
+// Optional: default export
+ export default shopMeta;
